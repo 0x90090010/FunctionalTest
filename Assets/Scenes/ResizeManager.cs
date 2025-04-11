cc0306f06c85acde10565f64138a7f866e2a6584
0x80090010 <0x90090010@gmail.com>
2025-04-11 21:35:43 +0900

    Update ResizeManager.cs
    imageRectTransform.sizeDeltaからimage.sprite.rectに変更

--- Assets/Scenes/ResizeManager.cs
+++ Assets/Scenes/ResizeManager.cs
@@ -18,7 +18,6 @@ public class ResizeManager : MonoBehaviour
 
     public void ResizeCanvas()
     {
-        // nullチェック
         if (canvasRectTransform != null)
         {
             // canvasのサイズをスクリーンサイズに合わせる
@@ -33,29 +32,84 @@ public class ResizeManager : MonoBehaviour
 
     public void ResizeImage()
     {
-        // nullチェック
+        // ResizeImageの仕様
+        // https://0x90090010.atlassian.net/wiki/pages/resumedraft.action?draftId=65701&draftShareId=754a3d08-0f33-434b-a9f4-289c5e82c1dd
+
+
+
         if (imageRectTransform != null)
         {
-            float ratio = -1;
+            Image image = imageRectTransform.GetComponent<Image>();
+            Debug.Log("size: " + image.sprite.rect.width + ", " + image.sprite.rect.height);
+            double ratio = 1;
+            int imageWidth = (int)image.sprite.rect.width;
+            int imageHeight = (int)image.sprite.rect.height;
+            int resizeWidth = 1;
+            int resizeHeight = 1;
 
-            // imageのサイズをスクリーンサイズに合わせる
-            if (Screen.width <= Screen.height)
+            if (Screen.width <= Screen.height && image.sprite.rect.width <= image.sprite.rect.height)             
             {
-                ratio = (float)Screen.width / (float)imageRectTransform.sizeDelta.x;
+                Debug.Log("Pattern A");
+                ratio = (double)Screen.width / (double)imageWidth;
+                Debug.Log("ratio: " + ratio);
+                resizeWidth = (int)(imageWidth * ratio);
+                resizeHeight = (int)(imageHeight * ratio);
             }
-            else
+            else if (Screen.width > Screen.height && image.sprite.rect.width > image.sprite.rect.height)
             {
-                ratio = (float)Screen.height / (float)imageRectTransform.sizeDelta.y;
+                Debug.Log("Pattern B");
+                ratio = (double)Screen.height / (double)imageHeight;
+                Debug.Log("ratio: " + ratio);
+                resizeWidth = (int)(imageWidth * ratio);
+                resizeHeight = (int)(imageHeight * ratio);
+
+                if (resizeWidth < Screen.width)
+                {
+                    Debug.Log("Pattern B - 1");
+                    Debug.Log("Width: " + resizeWidth + "," +  Screen.width);
+                    ratio = (double)Screen.width / (double)resizeWidth;
+                    resizeWidth = (int)(resizeWidth * ratio);
+                    resizeHeight = (int)(resizeHeight * ratio);
+                }
+            }
+            else if (Screen.width > Screen.height && image.sprite.rect.width <= image.sprite.rect.height)
+            {
+                Debug.Log("Pattern C");
+                ratio = (double)Screen.height / (double)imageHeight;
+                Debug.Log("ratio: " + ratio);
+                resizeWidth = (int)(imageWidth * ratio);
+                resizeHeight = (int)(imageHeight * ratio);
+
+                if (resizeWidth < Screen.width)
+                {
+                    Debug.Log("Pattern C - 1");
+                    Debug.Log("Width: " + resizeWidth + "," + Screen.width);
+                    ratio = (double)Screen.width / (double)resizeWidth;
+                    resizeWidth = (int)(resizeWidth * ratio);
+                    resizeHeight = (int)(resizeHeight * ratio);
+                }
+            }
+            else // (Screen.width <= Screen.height && imageRectTransform.sizeDelta.x > imageRectTransform.sizeDelta.y)
+            {
+                Debug.Log("Pattern D");
+                ratio = (double)Screen.width / (double)imageWidth;
+                Debug.Log("ratio: " + ratio);
+                resizeWidth = (int)(imageWidth * ratio);
+                resizeHeight = (int)(imageHeight * ratio);
+                Debug.Log("resize D: " + resizeWidth + ", " + resizeHeight);
+                if (resizeHeight < Screen.height)
+                {
+                    Debug.Log("Pattern D-1");
+                    Debug.Log("Height: " + resizeHeight + "," + Screen.height);
+                    ratio = (double)Screen.height / (double)resizeHeight;
+                    resizeWidth = (int)(resizeWidth * ratio);
+                    resizeHeight = (int)(resizeHeight * ratio);
+                    Debug.Log("resize D-1: " + resizeWidth + ", " + resizeHeight);
+                }
             }
 
-            // 画像のサイズを調整するための係数
-            float hoge = 1.01f;
 
-            // 画像のサイズを調整
-            int width = (int)(imageRectTransform.sizeDelta.x * ratio * hoge);
-            int height = (int)(imageRectTransform.sizeDelta.y * ratio * hoge);
-
-            imageRectTransform.sizeDelta = new Vector2(width, height);
+            imageRectTransform.sizeDelta = new Vector2(resizeWidth, resizeHeight);
             Debug.Log("Image resized!");
         }
         else
@@ -63,4 +117,5 @@ public class ResizeManager : MonoBehaviour
             Debug.LogError("Image RectTransform is null.");
         }
     }
+
 }
