a50474439792645232abc50e9f908d01676dab10
0x80090010 <0x90090010@gmail.com>
2025-04-12 01:47:04 +0900

    機能の変更と追加
    ・TitleSceneManager
    └ボタン表示機能を追加
    └ResizeManagerに合わせた改修
    
    ・ResizeManager
    └リサイズ機能を全画面で表示する画像をリサイズする機能という形に変更
    └全画面で表示しない画像をリサイズする機能を追加

--- Assets/Scenes/ResizeManager.cs
+++ Assets/Scenes/ResizeManager.cs
@@ -1,19 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ResizeManager : MonoBehaviour
 {
     private RectTransform canvasRectTransform;
-    private RectTransform imageRectTransform;
-    public TitleSceneManager fuga;
 
     void Start()
     {
         canvasRectTransform = GetComponent<RectTransform>();
-        imageRectTransform = GetComponent<RectTransform>();
-        Debug.Log(fuga);
     }
 
     public void ResizeCanvas()
@@ -30,92 +28,119 @@ public class ResizeManager : MonoBehaviour
         }
     }
 
-    public void ResizeImage()
+    /// <summary>
+    /// ResizeFullScreenImageOverCanvas
+    /// 主に全画面でCanvasより大きく表示したい画像に使用する
+    /// 
+    /// 仕様
+    /// https://0x90090010.atlassian.net/wiki/pages/resumedraft.action?draftId=65701&draftShareId=754a3d08-0f33-434b-a9f4-289c5e82c1dd
+    /// </summary>
+    public void ResizeFullScreenImageOverCanvas(List<RectTransform> imageRectTransforms)
     {
-        // ResizeImageの仕様
-        // https://0x90090010.atlassian.net/wiki/pages/resumedraft.action?draftId=65701&draftShareId=754a3d08-0f33-434b-a9f4-289c5e82c1dd
 
-
-
-        if (imageRectTransform != null)
+        foreach (RectTransform imageRectTransform in imageRectTransforms)
         {
-            Image image = imageRectTransform.GetComponent<Image>();
-            Debug.Log("size: " + image.sprite.rect.width + ", " + image.sprite.rect.height);
-            double ratio = 1;
-            int imageWidth = (int)image.sprite.rect.width;
-            int imageHeight = (int)image.sprite.rect.height;
-            int resizeWidth = 1;
-            int resizeHeight = 1;
 
-            if (Screen.width <= Screen.height && image.sprite.rect.width <= image.sprite.rect.height)             
+            if (imageRectTransform != null)
             {
-                Debug.Log("Pattern A");
-                ratio = (double)Screen.width / (double)imageWidth;
-                Debug.Log("ratio: " + ratio);
-                resizeWidth = (int)(imageWidth * ratio);
-                resizeHeight = (int)(imageHeight * ratio);
-            }
-            else if (Screen.width > Screen.height && image.sprite.rect.width > image.sprite.rect.height)
-            {
-                Debug.Log("Pattern B");
-                ratio = (double)Screen.height / (double)imageHeight;
-                Debug.Log("ratio: " + ratio);
-                resizeWidth = (int)(imageWidth * ratio);
-                resizeHeight = (int)(imageHeight * ratio);
+                Image image = imageRectTransform.GetComponent<Image>();
+                double ratio = 1;
+                int imageWidth = (int)image.sprite.rect.width;
+                int imageHeight = (int)image.sprite.rect.height;
+                int resizeWidth = 1;
+                int resizeHeight = 1;
 
-                if (resizeWidth < Screen.width)
+                if (Screen.width <= Screen.height && image.sprite.rect.width <= image.sprite.rect.height)
                 {
-                    Debug.Log("Pattern B - 1");
-                    Debug.Log("Width: " + resizeWidth + "," +  Screen.width);
-                    ratio = (double)Screen.width / (double)resizeWidth;
-                    resizeWidth = (int)(resizeWidth * ratio);
-                    resizeHeight = (int)(resizeHeight * ratio);
+                    Debug.Log("Pattern A");
+                    ratio = (double)Screen.width / (double)imageWidth;
+                    resizeWidth = (int)(imageWidth * ratio);
+                    resizeHeight = (int)(imageHeight * ratio);
+                    if (resizeHeight < Screen.height)
+                    {
+                        ratio = (double)Screen.height / (double)resizeHeight;
+                        resizeWidth = (int)(resizeWidth * ratio);
+                        resizeHeight = (int)(resizeHeight * ratio);
+                    }
                 }
-            }
-            else if (Screen.width > Screen.height && image.sprite.rect.width <= image.sprite.rect.height)
-            {
-                Debug.Log("Pattern C");
-                ratio = (double)Screen.height / (double)imageHeight;
-                Debug.Log("ratio: " + ratio);
-                resizeWidth = (int)(imageWidth * ratio);
-                resizeHeight = (int)(imageHeight * ratio);
-
-                if (resizeWidth < Screen.width)
+                else if (Screen.width > Screen.height && image.sprite.rect.width > image.sprite.rect.height)
                 {
-                    Debug.Log("Pattern C - 1");
-                    Debug.Log("Width: " + resizeWidth + "," + Screen.width);
-                    ratio = (double)Screen.width / (double)resizeWidth;
-                    resizeWidth = (int)(resizeWidth * ratio);
-                    resizeHeight = (int)(resizeHeight * ratio);
+                    Debug.Log("Pattern B");
+                    ratio = (double)Screen.height / (double)imageHeight;
+                    resizeWidth = (int)(imageWidth * ratio);
+                    resizeHeight = (int)(imageHeight * ratio);
+
+                    if (resizeWidth < Screen.width)
+                    {
+                        ratio = (double)Screen.width / (double)resizeWidth;
+                        resizeWidth = (int)(resizeWidth * ratio);
+                        resizeHeight = (int)(resizeHeight * ratio);
+                    }
                 }
-            }
-            else // (Screen.width <= Screen.height && imageRectTransform.sizeDelta.x > imageRectTransform.sizeDelta.y)
-            {
-                Debug.Log("Pattern D");
-                ratio = (double)Screen.width / (double)imageWidth;
-                Debug.Log("ratio: " + ratio);
-                resizeWidth = (int)(imageWidth * ratio);
-                resizeHeight = (int)(imageHeight * ratio);
-                Debug.Log("resize D: " + resizeWidth + ", " + resizeHeight);
-                if (resizeHeight < Screen.height)
+                else if (Screen.width > Screen.height && image.sprite.rect.width <= image.sprite.rect.height)
                 {
-                    Debug.Log("Pattern D-1");
-                    Debug.Log("Height: " + resizeHeight + "," + Screen.height);
-                    ratio = (double)Screen.height / (double)resizeHeight;
-                    resizeWidth = (int)(resizeWidth * ratio);
-                    resizeHeight = (int)(resizeHeight * ratio);
-                    Debug.Log("resize D-1: " + resizeWidth + ", " + resizeHeight);
+                    Debug.Log("Pattern C");
+                    ratio = (double)Screen.height / (double)imageHeight;
+                    resizeWidth = (int)(imageWidth * ratio);
+                    resizeHeight = (int)(imageHeight * ratio);
+
+                    if (resizeWidth < Screen.width)
+                    {
+                        ratio = (double)Screen.width / (double)resizeWidth;
+                        resizeWidth = (int)(resizeWidth * ratio);
+                        resizeHeight = (int)(resizeHeight * ratio);
+                    }
                 }
+                else // (Screen.width <= Screen.height && image.sprite.rect.width > image.sprite.rect.height)
+                {
+                    Debug.Log("Pattern D");
+                    ratio = (double)Screen.width / (double)imageWidth;
+                    resizeWidth = (int)(imageWidth * ratio);
+                    resizeHeight = (int)(imageHeight * ratio);
+                    if (resizeHeight < Screen.height)
+                    {
+                        ratio = (double)Screen.height / (double)resizeHeight;
+                        resizeWidth = (int)(resizeWidth * ratio);
+                        resizeHeight = (int)(resizeHeight * ratio);
+                    }
+                }
+
+
+                imageRectTransform.sizeDelta = new Vector2(resizeWidth, resizeHeight);
+                Debug.Log("Image resized!");
             }
-
-
-            imageRectTransform.sizeDelta = new Vector2(resizeWidth, resizeHeight);
-            Debug.Log("Image resized!");
-        }
-        else
-        {
-            Debug.LogError("Image RectTransform is null.");
+            else
+            {
+                Debug.LogError("Image RectTransform is null.");
+            }
         }
     }
 
+    /// <summary>
+    /// ResizeImageKeepingAspect
+    /// 主に全画面に表示しない画像（ボタンなど）に使用する
+    /// </summary>
+    public void ResizeImageKeepingAspect(Vector2 screenSize, List<RectTransform> imageRectTransforms)
+    {
+        foreach (RectTransform imageRectTransform in imageRectTransforms)
+        {
+            if (imageRectTransform != null)
+            {
+                double widthRatio = Screen.width / screenSize.x;
+                double heightRatio = Screen.height / screenSize.y;
+                double ratio = Math.Min(widthRatio, heightRatio);
+                Debug.Log("ResizeImageKeepingAspect: " + ratio);
+                if (imageRectTransform != null)
+                {
+                    Image image = imageRectTransform.GetComponent<Image>();
+                    int resizeWidth = (int)(image.sprite.rect.width * ratio);
+                    int resizeHeight = (int)(image.sprite.rect.height * ratio);
+
+                    imageRectTransform.sizeDelta = new Vector2(resizeWidth, resizeHeight);
+                    Debug.Log("Image resized And Keep aspect!");
+                }
+            }
+        }
+    }
+    
 }
--- Assets/Scenes/TitleSceneManager.cs
+++ Assets/Scenes/TitleSceneManager.cs
@@ -10,15 +10,26 @@ public class TitleSceneManager : MonoBehaviour
     private ResizeManager resizeCanvas;
     private ResizeManager resizeImage;
     private Vector2 screenSize;
-    private GameObject titleBackgroundCanvasObject;
-    public GameObject titleBackgroundImageObject;
+    private Vector2 defaultScreenSize;
+    private GameObject titleCanvasObject;
+    private Canvas titleCanvas;
+    private GameObject titleBackgroundImageObject;
+    private GameObject titleSampleButtonImageObject;
+    private List<RectTransform> imageRectTransforms;
+    private List<RectTransform> buttonsRectTransforms;
 
     void Start()
     {
+        imageRectTransforms = new List<RectTransform>();
+        buttonsRectTransforms = new List<RectTransform>();
+
+        TitleMain();
         SetTitleBackground();
-        resizeCanvas = titleBackgroundCanvasObject.AddComponent<ResizeManager>();
+        SetButton();
+        resizeCanvas = titleCanvasObject.AddComponent<ResizeManager>();
         resizeImage = titleBackgroundImageObject.AddComponent<ResizeManager>();
         screenSize = new Vector2(Screen.width, Screen.height);
+        defaultScreenSize = new Vector2(Screen.width, Screen.height); 
     }
 
     void Update()
@@ -44,7 +55,8 @@ public class TitleSceneManager : MonoBehaviour
             if (resizeImage != null)
             {
                 Debug.Log("image");
-                resizeImage.ResizeImage();
+                resizeImage.ResizeFullScreenImageOverCanvas(imageRectTransforms);
+                resizeImage.ResizeImageKeepingAspect(defaultScreenSize, buttonsRectTransforms);
                 screenSize = new Vector2(Screen.width, Screen.height);
             }
             else
@@ -56,23 +68,29 @@ public class TitleSceneManager : MonoBehaviour
         }
     }
 
-    void SetTitleBackground()
+    void TitleMain()
     {
         // Canvas用のゲームオブジェクトを作成
-        titleBackgroundCanvasObject = new GameObject("TitleBackgroundCanvasObject");
+        titleCanvasObject = new GameObject("TitleCanvasObject");
+        Debug.Log(titleCanvasObject);
 
         // Canvasコンポーネントを追加
-        Canvas titleBackgroundCanvas = titleBackgroundCanvasObject.AddComponent<Canvas>();
+        titleCanvas = titleCanvasObject.AddComponent<Canvas>();
+        Debug.Log(titleCanvas);
+
+        titleCanvasObject.AddComponent<GraphicRaycaster>();
 
         // レンダーモードをScreenSpaceCameraに設定
-        titleBackgroundCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-        titleBackgroundCanvasObject.AddComponent<GraphicRaycaster>();
+        titleCanvas.renderMode = RenderMode.ScreenSpaceCamera;
 
         // EventSystemを生成(ボタンやイベントで遷移などをする際には必ず必要)
         GameObject eventSystemObject = new GameObject("TitleEventSystem");
         eventSystemObject.AddComponent<EventSystem>();
         eventSystemObject.AddComponent<StandaloneInputModule>();
+    }
 
+    void SetTitleBackground()
+    {
         // image用のゲームオブジェクトを作成
         titleBackgroundImageObject = new GameObject("TitleBackgroundImage");
 
@@ -80,7 +98,7 @@ public class TitleSceneManager : MonoBehaviour
         Image titleBackgroundImage = titleBackgroundImageObject.AddComponent<Image>();
 
         // canvasの子要素に設定
-        titleBackgroundImageObject.transform.SetParent(titleBackgroundCanvas.transform, false);
+        titleBackgroundImageObject.transform.SetParent(titleCanvas.transform, false);
 
         // RectTransformの設定
         RectTransform rectTransform = titleBackgroundImageObject.GetComponent<RectTransform>();
@@ -89,11 +107,90 @@ public class TitleSceneManager : MonoBehaviour
         Texture2D titleBackgroundTexture = Resources.Load<Texture2D>("DefaultTitleBackground");
         Sprite sprite = Sprite.Create(titleBackgroundTexture, new Rect(0, 0, titleBackgroundTexture.width, titleBackgroundTexture.height), new Vector2(0.5f, 0.5f));
 
-        // 画像のサイズをRectTransformに設定
-        rectTransform.sizeDelta = new Vector2(titleBackgroundTexture.width, titleBackgroundTexture.height);
+        double ratio = 1;
+        int resizeWidth = 1;
+        int resizeHeight = 1;
+
+        if (Screen.width <= Screen.height && titleBackgroundTexture.width <= titleBackgroundTexture.height)
+        {
+            ratio = (double)Screen.width / (double)titleBackgroundTexture.width;
+            resizeWidth = (int)(titleBackgroundTexture.width * ratio);
+            resizeHeight = (int)(titleBackgroundTexture.height * ratio);
+
+            if (resizeHeight < Screen.height)
+            {
+                ratio = (double)Screen.height / (double)resizeHeight;
+                resizeWidth = (int)(resizeWidth * ratio);
+                resizeHeight = (int)(resizeHeight * ratio);
+            }
+        }
+        else if (Screen.width > Screen.height && titleBackgroundTexture.width > titleBackgroundTexture.height)
+        {
+            ratio = (double)Screen.height / (double)titleBackgroundTexture.height;
+            resizeWidth = (int)(titleBackgroundTexture.width * ratio);
+            resizeHeight = (int)(titleBackgroundTexture.height * ratio);
+
+            if (resizeWidth < Screen.width)
+            {
+                ratio = (double)Screen.width / (double)resizeWidth;
+                resizeWidth = (int)(resizeWidth * ratio);
+                resizeHeight = (int)(resizeHeight * ratio);
+            }
+        }
+        else if (Screen.width > Screen.height && titleBackgroundTexture.width <= titleBackgroundTexture.height)
+        {
+            ratio = (double)Screen.height / (double)titleBackgroundTexture.height;
+            resizeWidth = (int)(titleBackgroundTexture.width * ratio);
+            resizeHeight = (int)(titleBackgroundTexture.height * ratio);
+
+            if (resizeWidth < Screen.width)
+            {
+                ratio = (double)Screen.width / (double)resizeWidth;
+                resizeWidth = (int)(resizeWidth * ratio);
+                resizeHeight = (int)(resizeHeight * ratio);
+            }
+        }
+        else // (Screen.width <= Screen.height && titleBackgroundTexture.width > titleBackgroundTexture.height)
+        {
+            ratio = (double)Screen.width / (double)titleBackgroundTexture.width;
+            resizeWidth = (int)(titleBackgroundTexture.width * ratio);
+            resizeHeight = (int)(titleBackgroundTexture.height * ratio);
+
+            if (resizeHeight < Screen.height)
+            {
+                ratio = (double)Screen.height / (double)resizeHeight;
+                resizeWidth = (int)(resizeWidth * ratio);
+                resizeHeight = (int)(resizeHeight * ratio);
+            }
+        }
+
+        rectTransform.sizeDelta = new Vector2(resizeWidth, resizeHeight);
 
         // 画像を表示
         titleBackgroundImage.sprite = sprite;
+        imageRectTransforms.Add(rectTransform);
+
     }
 
+    void SetButton()
+    {
+        // image用のゲームオブジェクトを作成
+        titleSampleButtonImageObject = new GameObject("TitleSampleButtonImage");
+
+        // imageコンポーネントを追加
+        Image titleSampleButtonImage = titleSampleButtonImageObject.AddComponent<Image>();
+
+        // canvasの子要素に設定
+        titleSampleButtonImageObject.transform.SetParent(titleCanvas.transform, false);
+
+        // RectTransformの設定
+        RectTransform rectTransform = titleSampleButtonImageObject.GetComponent<RectTransform>();
+
+        // 仮のボタン
+        Texture2D titleSampleButtonTexture = Resources.Load<Texture2D>("SampleButton");
+        Sprite sprite = Sprite.Create(titleSampleButtonTexture, new Rect(0, 0, titleSampleButtonTexture.width, titleSampleButtonTexture.height), new Vector2(0.5f, 0.5f));
+        rectTransform.sizeDelta = new Vector2(titleSampleButtonTexture.width, titleSampleButtonTexture.height);
+        titleSampleButtonImage.sprite = sprite;
+        buttonsRectTransforms.Add(rectTransform);
+}
 }
